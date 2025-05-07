4d6d41ea434fda95cbea9e11ec395165b84ec7e8
CALEB OUEDRAOGO <calebouedraogo@macbookpro.mynetworksettings.com>
2025-05-07 14:07:23 -0400

    Added createForm method to handle new tasks creation

--- taskmanager/src/main/java/com/caleb/taskmanager/controller/TaskController.java
+++ taskmanager/src/main/java/com/caleb/taskmanager/controller/TaskController.java
@@ -1,8 +1,10 @@
 package com.caleb.taskmanager.controller;
 
+import com.caleb.taskmanager.model.Task;
 import com.caleb.taskmanager.repository.TaskRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TaskController {
@@ -11,8 +13,16 @@ public class TaskController {
         this.taskRepo = taskRepo;
     }
 
+    @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("tasks", taskRepo.findAll());
-        return index;
+        return "index";
     }
+
+    @GetMapping("/tasks/new")
+    public String createForm(Model model) {
+        model.addAttribute("task", new Task());
+        return "create";
+    }
+
 }
